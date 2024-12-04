d2894248d4878376497cc24ad47456e958cfa9ce
Bas Dijkstra <bas@ontestautomation.com>
2024-12-04 13:19:45 +0100

    Add LoanRequest model

---
+++ ImprovingYourTestAutomationCode/Models/LoanRequest.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace ImprovingYourTestAutomationCode.Models
+{
+    public class LoanRequest
+    {
+        [JsonProperty("loanAmount")]
+        public string LoanAmount { get; set; }
+
+        [JsonProperty("downPayment")]
+        public string DownPayment { get; set; }
+
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+    }
+}
